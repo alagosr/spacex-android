963a53310516ac438ea1f93cd077f33381f00220
Alejandro Lagos <flagos.rd@gmail.com>
2022-07-27 13:13:14 -0500

    Changes variable names in LaunchItem.kt

--- app/src/main/java/com/flagos/spacex/domain/LaunchItem.kt
+++ app/src/main/java/com/flagos/spacex/domain/LaunchItem.kt
@@ -7,25 +7,24 @@ import com.squareup.moshi.JsonClass
 data class LaunchItem(
     @field:Json(name = "flight_number") val flightNumber: Int,
     @field:Json(name = "mission_name") val missionName: String,
-    @field:Json(name = "launch_year") val launchYear: String,
-    @field:Json(name = "launch_date_utc") val launchDateUtc: String,
+    @field:Json(name = "launch_date_unix") val dateInUnix: Long,
     @field:Json(name = "rocket") val rocket: RocketItem,
-    @field:Json(name = "launch_site") val siteId: LaunchSiteItem,
+    @field:Json(name = "launch_site") val site: LaunchSiteItem,
     @field:Json(name = "details") val details: String?,
     @field:Json(name = "links") val links: LinksItem,
 )
 
 @JsonClass(generateAdapter = true)
 data class RocketItem(
-    @field:Json(name = "rocket_id") val rocketId: String,
-    @field:Json(name = "rocket_name") val rocketName: String,
-    @field:Json(name = "rocket_type") val rocketType: String?,
+    @field:Json(name = "rocket_id") val id: String,
+    @field:Json(name = "rocket_name") val name: String,
+    @field:Json(name = "rocket_type") val type: String?,
 )
 
 @JsonClass(generateAdapter = true)
 data class LaunchSiteItem(
-    @field:Json(name = "site_id") val siteId: String,
-    @field:Json(name = "site_name") val siteName: String,
+    @field:Json(name = "site_id") val id: String,
+    @field:Json(name = "site_name_long") val name: String,
 )
 
 @JsonClass(generateAdapter = true)
